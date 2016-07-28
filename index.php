78b9f598efdceea6e94c7dc23d9d8c1f1ecc2da7
Jonatas Fil <ctfninj4@gmail.com>
2016-07-28 19:24:17 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<html><div name="pattern">
+	<head>
+		<link rel="shortcut icon" href="http://f1.bcbits.com/img/a2328193370_10.jpg">
+	<title>Ninj4</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Brazilian Player of Capture The Flag">
+	<meta name="author" content="Jonatas Fil">
+	<meta name="CTF Team Nationality" content="Brazil">
+	<meta name="CTF Player Name" content="Ninj4">
+	</head>
+<body><center>
+	<pre>
+	<font size="">
+		<center><font style="text-shadow: #33FF00 0px 0px 20px" color="Red">#<br /><marquee behavior="alternate" scrollamount="5" scrolldelay="-0%" width="80%"><font color="Yellow"  size="">Welcome !!!</font></marquee><br />#<pre></center>   
+ ███▄    █  ██▓ ███▄    █  ▄▄▄██▀▀▀▄▄▄      
+ ██ ▀█   █ ▓██▒ ██ ▀█   █    ▒██  ▒████▄    
+▓██  ▀█ ██▒▒██▒▓██  ▀█ ██▒   ░██  ▒██  ▀█▄  
+▓██▒  ▐▌██▒░██░▓██▒  ▐▌██▒▓██▄██▓ ░██▄▄▄▄██ 
+▒██░   ▓██░░██░▒██░   ▓██░ ▓███▒   ▓█   ▓██▒
+░ ▒░   ▒ ▒ ░▓  ░ ▒░   ▒ ▒  ▒▓▒▒░   ▒▒   ▓▒█░
+░ ░░   ░ ▒░ ▒ ░░ ░░   ░ ▒░ ▒ ░▒░    ▒   ▒▒ ░
+   ░   ░ ░  ▒ ░   ░   ░ ░  ░ ░ ░    ░   ▒   
+         ░  ░           ░  ░   ░        ░  ░             
+        </pre>		
+	</font></center><hr>
+	<center>
+<body background="http://cdn.wonderfulengineering.com/wp-content/uploads/2014/04/code-wallpaper-17.jpg"></body>
+</center>
+<br>
+<center>
+<img src="http://corgiorgy.com/corgiswimflipflip.gif"> 
+</center>
+<STYLE>
+<!--
+A{text-decoration:none}
+-->
+</STYLE>
+<STYLE>
+a { color:black } /* Globally */
+
+/* Each state */
+
+a:visited { text-decoration: none; color:black; }
+a:hover { text-decoration: none; color:blue; }
+a:focus { text-decoration: none; color:blue; }
+a:hover, a:active { text-decoration: none; color:black }
+</STYLE>
+</body>
+</html>
+<iframe allowfullscreen="" frameborder="0" height="1" src="https://www.youtube.com/embed/hfjekgOgAFQ?autoplay=1" width="1"></iframe></p>
